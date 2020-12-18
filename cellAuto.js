aca5d1cc80e5793d8ad1bb7861ac31da857ff184
SebasBar <toolt2y@gmail.com>
2020-12-18 14:23:49 +0100

    first try

---
+++ cellAuto.js
@@ -0,0 +1,128 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const tile = 5;
+const canvaHeight = 1800;
+const canvaWidth = 3600;
+
+const canvaGridXNum = Math.round(canvaWidth / tile);
+const canvaGridYNum = Math.round(canvaHeight / tile);
+
+const middleCell = Math.round(canvaGridXNum / 2);
+
+console.log("canvagridX", canvaGridXNum);
+
+const lineCounter = 1;
+
+const newLineCells = [];
+
+//const rule = [0, 0, 0, 1, 1, 1, 1, 0]; // rule 30
+// const rule = [0, 1, 1, 1, 1, 1, 1, 0];
+// const rule = [0, 0, 0, 1, 1, 1, 1, 0];
+//const rule = [0, 0, 1, 0, 1, 0, 0, 1]; // rule 41
+const rule = [1, 0, 1, 1, 0, 1, 1, 0]; // rule 182
+
+const ruleDef = [
+  [1, 1, 1],
+  [1, 1, 0],
+  [1, 0, 1],
+  [1, 0, 0],
+  [0, 1, 1],
+  [0, 1, 0],
+  [0, 0, 1],
+  [0, 0, 0],
+];
+
+const drawBoard = () => {
+  for (x = 0; x < canvaWidth; x = x + tile) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvaHeight);
+  }
+  for (y = 0; y < canvaHeight; y = y + tile) {
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvaWidth, y);
+  }
+  ctx.strokeStyle = "#333333";
+  ctx.stroke();
+};
+
+const drawCell = (x, y) => {
+  ctx.fillStyle = "black";
+  ctx.fillRect((x - 1) * tile, tile * (y - 1), tile, tile);
+};
+
+const checkCellAlive = (x, y) => {
+  const pixelData = ctx.getImageData(
+    x * tile - tile / 2,
+    y * tile - tile / 2,
+    1,
+    1
+  );
+
+  if (pixelData.data[3] === 255) {
+    return 1;
+  } else return 0;
+};
+
+const checkThreePrevNight = (x, y) => {
+  const threePrev = [null, null, null];
+  if (checkCellAlive(x - 1, y - 1) === 1) {
+    threePrev[0] = 1;
+  } else threePrev[0] = 0;
+  if (checkCellAlive(x, y - 1) === 1) {
+    threePrev[1] = 1;
+  } else threePrev[1] = 0;
+  if (checkCellAlive(x + 1, y - 1) === 1) {
+    threePrev[2] = 1;
+  } else threePrev[2] = 0;
+
+  return threePrev;
+};
+
+const createCell = (x, y) => {
+  for (i = 0; i < 8; i++) {
+    if (rule[i] === 1) {
+      if (
+        ruleDef[i][0] === checkThreePrevNight(x, y)[0] &&
+        ruleDef[i][1] === checkThreePrevNight(x, y)[1] &&
+        ruleDef[i][2] === checkThreePrevNight(x, y)[2]
+      ) {
+        drawCell(x, y);
+        return;
+      }
+    }
+  }
+};
+
+const oddToHalf = (num) => {
+  return parseInt(num / 2);
+};
+
+const optimizedCreateLine = (y) => {
+  for (
+    var k = -1 * oddToHalf(2 * (y - 1) + 1);
+    k <= oddToHalf(2 * (y - 1) + 1);
+    k++
+  ) {
+    createCell(middleCell - k, y);
+  }
+};
+
+const createLine = (y) => {
+  for (var k = 0; k < canvaGridXNum; k++) {
+    createCell(k + 1, y);
+  }
+};
+
+const animation = () => {
+  for (var j = 1; j < canvaGridYNum; j++) {
+    //createLine(j + 1);
+    optimizedCreateLine(j + 1);
+  }
+};
+
+firstCell = drawCell(middleCell, 1);
+
+animation();
+
+drawBoard();
